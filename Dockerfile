c6623dfcdf29327cdff3ef72ac18e9160c4da919
vargamolnarbertalan <vargamolnarb@gmail.com>
2025-03-27 23:54:35 +0100

    0327 save

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # Use a lightweight Python base image
 FROM python:3.11-slim
 
+ENV TZ="Europe/Budapest"
+
 # Install system dependencies
 RUN apt update && apt install -y ffmpeg
 
@@ -16,8 +18,19 @@ COPY watch-folder.sh /app/
 
 COPY config.env /app/
 
+COPY index.html /app/
+
+COPY websocat.x86_64-unknown-linux-musl /usr/local/bin/websocat
+
 # Give execution permissions to the script
 RUN chmod +x /app/watch-folder.sh
 
+#RUN websocat -s 4981 &
+
+#RUN python3 -m http.server 4980 &
+
+EXPOSE 4980 4981
+
 # Start the watcher script on container launch
 CMD ["/app/watch-folder.sh"]
+#CMD ["echo", "hello"]
--- watch-folder.sh
+++ watch-folder.sh
@@ -67,7 +67,7 @@ is_file_complete() {
 check_extension() {
   filename="$1"
   extension="${filename##*.}"
-  echo "#### extension: $extension ####"
+  echo "#### extension: $extension ####" | websocat ws://0.0.0.0:4981
   [[ "$extension" =~ ^($ALLOWED_INPUT_FORMATS)$ ]] && return 0 || return 1
 }
 
@@ -76,22 +76,25 @@ mkdir -p "$(dirname "$LOG_FILE")"
 mkdir -p "$(dirname "$LOG_FILE2")"
 mkdir -p "$(dirname "$LOG_FILE3")"
 
-echo "$(date) - Watching directory: $WATCH_DIR for new files..." | tee -a "$LOG_FILE"
-echo "$(date) - Watching directory: $WATCH_DIR2 for new files..." | tee -a "$LOG_FILE2"
-echo "$(date) - Watching directory: $WATCH_DIR3 for new files..." | tee -a "$LOG_FILE3"
+websocat -t ws-l:0.0.0.0:4981 -E broadcast:mirror: &
+python3 -m http.server 4980 &
+
+echo "$(date) - Watching directory: $WATCH_DIR for new files..." | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
+echo "$(date) - Watching directory: $WATCH_DIR2 for new files..." | tee -a "$LOG_FILE2" | websocat ws://0.0.0.0:4981
+echo "$(date) - Watching directory: $WATCH_DIR3 for new files..." | tee -a "$LOG_FILE3" | websocat ws://0.0.0.0:4981
 
 while true; do
   ############### watch dir 1 ##################
   for FILE in "$WATCH_DIR"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
         out_basename="${in_basename}_encoded_${PRESET_NAME}"
 
-        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         #echo "Input file basename: $in_basename"
         #echo "Output file basename: $out_basename"
         out_path="${OUTPUT_DIR}/${out_basename}.${OUT_EXTENSION}"
@@ -103,24 +106,24 @@ while true; do
 
         # If audio stream exists, normalize audio and encode video
         if [ -n "$AUDIO_STREAM" ]; then
-          /app/venv/bin/ffmpeg-normalize "$FILE" -o "$out_path" -ext "$OUT_EXTENSION" -e="$VIDEO_OPTIONS" -c:a "$AUDIO_CODEC" -b:a "$AUDIO_BITRATE" -nt "$STANDARD" -t "$TARGET_LOUDNESS" --dual-mono -ar "$SAMPLE_RATE" -v | tee -a "$LOG_FILE"
+          /app/venv/bin/ffmpeg-normalize "$FILE" -o "$out_path" -ext "$OUT_EXTENSION" -e="$VIDEO_OPTIONS" -c:a "$AUDIO_CODEC" -b:a "$AUDIO_BITRATE" -nt "$STANDARD" -t "$TARGET_LOUDNESS" --dual-mono -ar "$SAMPLE_RATE" -v | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         else
           # If no audio stream, just encode the video stream
-          ffmpeg -i "$FILE" -c:v "$VIDEO_ENCODER" -b:v "$VIDEO_BITRATE" -r "$FRAME_RATE" -s "$RESOLUTION" -c:a copy "$out_path.$OUT_EXTENSION" -loglevel verbose | tee -a "$LOG_FILE"
+          ffmpeg -i "$FILE" -c:v "$VIDEO_ENCODER" -b:v "$VIDEO_BITRATE" -r "$FRAME_RATE" -s "$RESOLUTION" -c:a copy "$out_path.$OUT_EXTENSION" -loglevel verbose | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         fi
 
-        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
 
         # move file
         mkdir -p "$WATCH_DIR/_original"
         mv "$FILE" "$WATCH_DIR/_original/"
       else
-        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         continue # Skip this file and check again later
       fi
     else
       if [[ -f "$FILE" ]]; then
-        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         # move file
         mkdir -p "$WATCH_DIR/_original"
         mv "$FILE" "$WATCH_DIR/_original/"
@@ -131,13 +134,13 @@ while true; do
   for FILE in "$WATCH_DIR2"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
         out_basename="${in_basename}_encoded_${PRESET_NAME2}"
 
-        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE2"
+        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE2" | websocat ws://0.0.0.0:4981
         #echo "Input file basename: $in_basename"
         #echo "Output file basename: $out_basename"
         out_path="${OUTPUT_DIR2}/${out_basename}.${OUT_EXTENSION2}"
@@ -149,24 +152,24 @@ while true; do
 
         # If audio stream exists remove audio and encode video
         if [ -n "$AUDIO_STREAM" ]; then
-          ffmpeg -i "$FILE" -an -c:v "$VIDEO_ENCODER2" -b:v "$VIDEO_BITRATE2" -r "$FRAME_RATE2" -s "$RESOLUTION2" "$out_path.$OUT_EXTENSION2" -loglevel verbose | tee -a "$LOG_FILE2"
+          ffmpeg -i "$FILE" -an -c:v "$VIDEO_ENCODER2" -b:v "$VIDEO_BITRATE2" -r "$FRAME_RATE2" -s "$RESOLUTION2" "$out_path.$OUT_EXTENSION2" -loglevel verbose | tee -a "$LOG_FILE2" | websocat ws://0.0.0.0:4981
         else
           # If no audio stream, just encode the video stream
-          ffmpeg -i "$FILE" -c:v "$VIDEO_ENCODER2" -b:v "$VIDEO_BITRATE2" -r "$FRAME_RATE2" -s "$RESOLUTION2" -c:a copy "$out_path.$OUT_EXTENSION2" -loglevel verbose | tee -a "$LOG_FILE2"
+          ffmpeg -i "$FILE" -c:v "$VIDEO_ENCODER2" -b:v "$VIDEO_BITRATE2" -r "$FRAME_RATE2" -s "$RESOLUTION2" -c:a copy "$out_path.$OUT_EXTENSION2" -loglevel verbose | tee -a "$LOG_FILE2" | websocat ws://0.0.0.0:4981
         fi
 
-        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE2"
+        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE2" | websocat ws://0.0.0.0:4981
 
         # move file
         mkdir -p "$WATCH_DIR2/_original"
         mv "$FILE" "$WATCH_DIR2/_original/"
       else
-        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         continue # Skip this file and check again later
       fi
     else
       if [[ -f "$FILE" ]]; then
-        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         # move file
         mkdir -p "$WATCH_DIR2/_original"
         mv "$FILE" "$WATCH_DIR2/_original/"
@@ -177,13 +180,13 @@ while true; do
   for FILE in "$WATCH_DIR3"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
         out_basename="${in_basename}_encoded_${PRESET_NAME3}"
 
-        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE3"
+        echo "$(date) - Processing file: $FILE" | tee -a "$LOG_FILE3" | websocat ws://0.0.0.0:4981
         #echo "Input file basename: $in_basename"
         #echo "Output file basename: $out_basename"
         out_path="${OUTPUT_DIR3}/${out_basename}.${OUT_EXTENSION3}"
@@ -195,24 +198,24 @@ while true; do
 
         # If audio stream exists, normalize audio and remove video stream
         if [ -n "$AUDIO_STREAM" ]; then
-          /app/venv/bin/ffmpeg-normalize "$FILE" -o "$out_path" -ext "$OUT_EXTENSION3" -vn -c:a "$AUDIO_CODEC3" -b:a "$AUDIO_BITRATE3" -nt "$STANDARD3" -t "$TARGET_LOUDNESS3" --dual-mono -ar "$SAMPLE_RATE3" -v | tee -a "$LOG_FILE3"
+          /app/venv/bin/ffmpeg-normalize "$FILE" -o "$out_path" -ext "$OUT_EXTENSION3" -vn -c:a "$AUDIO_CODEC3" -b:a "$AUDIO_BITRATE3" -nt "$STANDARD3" -t "$TARGET_LOUDNESS3" --dual-mono -ar "$SAMPLE_RATE3" -v | tee -a "$LOG_FILE3" | websocat ws://0.0.0.0:4981
         else
           # If no audio stream, log it and move file
-          echo "No audio stream found, can't output normalized audio file." | tee -a "$LOG_FILE3"
+          echo "No audio stream found, can't output normalized audio file." | tee -a "$LOG_FILE3" | websocat ws://0.0.0.0:4981
         fi
 
-        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE3"
+        echo "$(date) - Finished processing: $FILE" | tee -a "$LOG_FILE3" | websocat ws://0.0.0.0:4981
 
         # move file
         mkdir -p "$WATCH_DIR3/_original"
         mv "$FILE" "$WATCH_DIR3/_original/"
       else
-        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - File is still being written: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         continue # Skip this file and check again later
       fi
     else
       if [[ -f "$FILE" ]]; then
-        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE"
+        echo "$(date) - Not supported input format: $FILE" | tee -a "$LOG_FILE" | websocat ws://0.0.0.0:4981
         # move file
         mkdir -p "$WATCH_DIR3/_original"
         mv "$FILE" "$WATCH_DIR3/_original/"
