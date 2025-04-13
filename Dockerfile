ce22b6e2c6d294d2138a24f3b7562eea9c476450
vargamolnarbertalan <vargamolnarb@gmail.com>
2025-04-13 15:47:05 +0200

    Env variables, removed some echoes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,42 @@
 # Use a lightweight Python base image
 FROM python:3.10-slim
 
-ENV TZ="Europe/Budapest"
+# Set environment variables
+ENV TZ=Europe/Budapest \ 
+    WATCH_DIR=/watch \
+    OUTPUT_DIR=/output \
+    LOG_FILE=/logs/normalization.log \
+    PRESET_NAME=EBU \
+    TARGET_LOUDNESS=-25 \
+    OUT_EXTENSION=mp4 \
+    AUDIO_BITRATE=256k \
+    STANDARD=ebu \
+    SAMPLE_RATE=48000 \
+    AUDIO_CODEC=aac \
+    VIDEO_ENCODER=libx264 \
+    VIDEO_BITRATE=10000k \
+    FRAME_RATE=60000/1001 \
+    RESOLUTION=1920x1080 \
+    WATCH_DIR2=/watch2 \
+    OUTPUT_DIR2=/output \
+    LOG_FILE2=/logs/normalization2.log \
+    PRESET_NAME2=MUTE \
+    OUT_EXTENSION2=mp4 \
+    VIDEO_ENCODER2=libx264 \
+    VIDEO_BITRATE2=10000k \
+    FRAME_RATE2=60000/1001 \
+    RESOLUTION2=1920x1080 \
+    WATCH_DIR3=/watch3 \
+    OUTPUT_DIR3=/output \
+    LOG_FILE3=/logs/normalization3.log \
+    PRESET_NAME3=audioEBU \
+    TARGET_LOUDNESS3=-25 \
+    OUT_EXTENSION3=mp3 \
+    AUDIO_BITRATE3=256k \
+    STANDARD3=ebu \
+    SAMPLE_RATE3=48000 \
+    AUDIO_CODEC3=libmp3lame \
+    ALLOWED_INPUT_FORMATS=mp4|webm|flv|ogg|gif|wmv|mts|m2ts|ts|m4v|mpg|mpeg|m4v|3gp|mxf|mkv|avi|mov|wav|aac|aiff|flac|mp3|mogg|wma
 
 # Install system dependencies
 RUN apt update && apt install -y ffmpeg bash
@@ -17,8 +52,6 @@ WORKDIR /app
 
 # Copy the watch folder script
 COPY watch-folder.sh /app/
-
-COPY config.env /app/
 
 COPY index.html /app/
 
--- watch-folder.sh
+++ watch-folder.sh
@@ -1,45 +1,4 @@
 #!/bin/bash
-source config.env
-
-WATCH_DIR=$(echo -n "$WATCH_DIR" | tr -d '\r')
-OUTPUT_DIR=$(echo -n "$OUTPUT_DIR" | tr -d '\r')
-LOG_FILE=$(echo -n "$LOG_FILE" | tr -d '\r')
-PRESET_NAME=$(echo -n "$PRESET_NAME" | tr -d '\r')
-TARGET_LOUDNESS=$(echo -n "$TARGET_LOUDNESS" | tr -d '\r')
-OUT_EXTENSION=$(echo -n "$OUT_EXTENSION" | tr -d '\r')
-AUDIO_BITRATE=$(echo -n "$AUDIO_BITRATE" | tr -d '\r')
-STANDARD=$(echo -n "$STANDARD" | tr -d '\r')
-SAMPLE_RATE=$(echo -n "$SAMPLE_RATE" | tr -d '\r')
-AUDIO_CODEC=$(echo -n "$AUDIO_CODEC" | tr -d '\r')
-VIDEO_ENCODER=$(echo -n "$VIDEO_ENCODER" | tr -d '\r')
-VIDEO_BITRATE=$(echo -n "$VIDEO_BITRATE" | tr -d '\r')
-FRAME_RATE=$(echo -n "$FRAME_RATE" | tr -d '\r')
-RESOLUTION=$(echo -n "$RESOLUTION" | tr -d '\r')
-VIDEO_OPTIONS="-c:v ${VIDEO_ENCODER} -b:v ${VIDEO_BITRATE} -r ${FRAME_RATE} -s ${RESOLUTION}"
-
-WATCH_DIR2=$(echo -n "$WATCH_DIR2" | tr -d '\r')
-OUTPUT_DIR2=$(echo -n "$OUTPUT_DIR2" | tr -d '\r')
-LOG_FILE2=$(echo -n "$LOG_FILE2" | tr -d '\r')
-PRESET_NAME2=$(echo -n "$PRESET_NAME2" | tr -d '\r')
-OUT_EXTENSION2=$(echo -n "$OUT_EXTENSION2" | tr -d '\r')
-VIDEO_ENCODER2=$(echo -n "$VIDEO_ENCODER2" | tr -d '\r')
-VIDEO_BITRATE2=$(echo -n "$VIDEO_BITRATE2" | tr -d '\r')
-FRAME_RATE2=$(echo -n "$FRAME_RATE2" | tr -d '\r')
-RESOLUTION2=$(echo -n "$RESOLUTION2" | tr -d '\r')
-VIDEO_OPTIONS2="-c:v ${VIDEO_ENCODER2} -b:v ${VIDEO_BITRATE2} -r ${FRAME_RATE2} -s ${RESOLUTION2}"
-
-WATCH_DIR3=$(echo -n "$WATCH_DIR3" | tr -d '\r')
-OUTPUT_DIR3=$(echo -n "$OUTPUT_DIR3" | tr -d '\r')
-LOG_FILE3=$(echo -n "$LOG_FILE3" | tr -d '\r')
-PRESET_NAME3=$(echo -n "$PRESET_NAME3" | tr -d '\r')
-TARGET_LOUDNESS3=$(echo -n "$TARGET_LOUDNESS3" | tr -d '\r')
-OUT_EXTENSION3=$(echo -n "$OUT_EXTENSION3" | tr -d '\r')
-AUDIO_BITRATE3=$(echo -n "$AUDIO_BITRATE3" | tr -d '\r')
-STANDARD3=$(echo -n "$STANDARD3" | tr -d '\r')
-SAMPLE_RATE3=$(echo -n "$SAMPLE_RATE3" | tr -d '\r')
-AUDIO_CODEC3=$(echo -n "$AUDIO_CODEC3" | tr -d '\r')
-
-ALLOWED_INPUT_FORMATS=$(echo -n "$ALLOWED_INPUT_FORMATS" | tr -d '\r')
 
 is_file_complete() {
   local file="$1"
@@ -68,7 +27,7 @@ check_extension() {
   filename="$1"
   extension="${filename##*.}"
   extension_lower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
-  echo "#### extension: $extension_lower ####" | websocat -1 ws://localhost:443/ws
+  # echo "#### extension: $extension_lower ####" | websocat -1 ws://localhost:443/ws
   [[ "$extension_lower" =~ ^($ALLOWED_INPUT_FORMATS)$ ]] && return 0 || return 1
 }
 
@@ -89,7 +48,7 @@ while true; do
   for FILE in "$WATCH_DIR"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
+        #echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
@@ -135,7 +94,7 @@ while true; do
   for FILE in "$WATCH_DIR2"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
+        #echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
@@ -181,7 +140,7 @@ while true; do
   for FILE in "$WATCH_DIR3"/*; do
     if [[ -f "$FILE" ]] && check_extension "$FILE"; then
       if is_file_complete "$FILE"; then
-        echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
+        #echo "$(date) - File is complete: $FILE" | tee -a "$LOG_FILE" | websocat -1 ws://localhost:443/ws
 
         filename=$(basename "$FILE")
         in_basename="${filename%.*}"
